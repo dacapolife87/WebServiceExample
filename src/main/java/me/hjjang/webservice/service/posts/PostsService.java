32f2b1f8e4bf5026bbe42f6b54b46eff932d246e
hJJANG <dacapolife87@gmail.com>
2021-02-23 08:36:25 +0900

    feat : 게시글 CRUD기능추가

--- src/main/java/me/hjjang/webservice/service/posts/PostsService.java
+++ src/main/java/me/hjjang/webservice/service/posts/PostsService.java
@@ -3,11 +3,16 @@ package me.hjjang.webservice.service.posts;
 import lombok.RequiredArgsConstructor;
 import me.hjjang.webservice.domain.posts.Posts;
 import me.hjjang.webservice.domain.posts.PostsRepository;
+import me.hjjang.webservice.web.dto.PostsListResponseDto;
 import me.hjjang.webservice.web.dto.PostsResponseDto;
 import me.hjjang.webservice.web.dto.PostsSaveRequestDto;
 import me.hjjang.webservice.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -38,5 +43,20 @@ public class PostsService {
                         new IllegalArgumentException("해당게시글이 없습니다. id = "+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
